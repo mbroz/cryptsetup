ec3b767103a42b213612834a8ef7f638efde1e61
Milan Broz <gmazyland@gmail.com>
2010-10-26 14:35:06 +0000

    Remove dead parse code.
    git-svn-id: https://cryptsetup.googlecode.com/svn/trunk@349 36d66b0a-2a48-0410-832c-cd162a569da5

--- lib/utils_crypt.c
+++ lib/utils_crypt.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <errno.h>
 
-#include "internal.h"
+#include "utils_crypt.h"
 
 int crypt_parse_name_and_mode(const char *s, char *cipher, char *cipher_mode)
 {
@@ -18,25 +18,3 @@ int crypt_parse_name_and_mode(const char *s, char *cipher, char *cipher_mode)
 
 	return -EINVAL;
 }
-
-#if 0
-/* Token content stringification, see info cpp/stringification */
-#define str(s) #s
-#define xstr(s) str(s)
-#define scanpattern1 "%" xstr(MAX_CIPHER_LEN) "[^-]-%" xstr(MAX_CIPHER_LEN)  "s"
-#define scanpattern2 "%" xstr(MAX_CIPHER_LEN) "[^-]"
-
-	if(sscanf(nameAndMode,scanpattern1, name, mode) != 2) {
-		if((r = sscanf(nameAndMode,scanpattern2,name)) == 1)
-			strncpy(mode,"cbc-plain",10);
-		else
-			return -EINVAL;
-	}
-
-	return 0;
-
-#undef scanpattern1
-#undef scanpattern2
-#undef str
-#undef xstr
-#endif
