56e73526add00399cbe9b20bb52105d77218b1f1
Milan Broz <gmazyland@gmail.com>
2011-10-09 21:05:20 +0000

    Add license to example files (LGPL).
    git-svn-id: https://cryptsetup.googlecode.com/svn/trunk@632 36d66b0a-2a48-0410-832c-cd162a569da5

--- docs/examples/crypt_luks_usage.c
+++ docs/examples/crypt_luks_usage.c
@@ -1,3 +1,23 @@
+/*
+ * An example of using LUKS device through libcryptsetup API
+ *
+ * Copyright (C) 2011, Red Hat, Inc. All rights reserved.
+ *
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
