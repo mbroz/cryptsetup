7eccb7ff5031a4f42f1ae8f7ffaefe80ba0d53dd
Milan Broz <gmazyland@gmail.com>
2012-12-29 11:43:52 +0100

    Change License for sub-libraries from GPLv2 only to LGPLv2.1+ ("or any later")
    This includes crypto, loopaes, tcrypt handling code I have written myself
    and verity code written originally by Mikulas Patocka and modified by me,
    copyright by Red Hat.
    
    Other part of library have to stay GPLv2+ only for now
    (no agreement from all authors).

--- lib/crypto_backend/crypto_openssl.c
+++ lib/crypto_backend/crypto_openssl.c
@@ -4,17 +4,18 @@
  * Copyright (C) 2010-2012, Red Hat, Inc. All rights reserved.
  * Copyright (C) 2010-2012, Milan Broz
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  * In addition, as a special exception, the copyright holders give
@@ -23,7 +24,7 @@
  * individual source file, and distribute linked combinations
  * including the two.
  *
- * You must obey the GNU General Public License in all respects
+ * You must obey the GNU Lesser General Public License in all respects
  * for all of the code used other than OpenSSL.
  */
 
