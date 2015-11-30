d3316ec930ce5ef4c53bd2ce61e344646e81177f
Ondrej Kozina <okozina@redhat.com>
2015-11-30 16:44:22 +0100

    Slight code style improvements for blockwise functions.

--- lib/internal.h
+++ lib/internal.h
@@ -104,7 +104,7 @@ uint64_t crypt_dev_partition_offset(const char *dev_path);
 ssize_t write_buffer(int fd, const void *buf, size_t count);
 ssize_t read_buffer(int fd, void *buf, size_t count);
 ssize_t write_blockwise(int fd, int bsize, void *buf, size_t count);
-ssize_t read_blockwise(int fd, int bsize, void *_buf, size_t count);
+ssize_t read_blockwise(int fd, int bsize, void *buf, size_t count);
 ssize_t write_lseek_blockwise(int fd, int bsize, char *buf, size_t count, off_t offset);
 ssize_t read_lseek_blockwise(int fd, int bsize, char *buf, size_t count, off_t offset);
 
