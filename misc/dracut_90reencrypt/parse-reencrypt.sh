ffa035f45ebd8d548f92415dc04b5148f6102e99
Harald Hoyer <harald@redhat.com>
2017-04-21 13:56:09 +0200

    dracut-reencrypt: add UUID handling to rd.luks.reencrypt=
    This patch adds a udev rule, so that you can specify
    rd.luks.reencrypt=<UUID> instead of rd.luks.reencrypt=<devname>
    
    It also moves the job to the "settled" queue, which means, that it is
    executed after udev has settled.

--- misc/dracut_90reencrypt/parse-reencrypt.sh
+++ misc/dracut_90reencrypt/parse-reencrypt.sh
@@ -18,9 +18,14 @@ if [ -n "$REENC_DEV" ] ; then
 {
    printf 'SUBSYSTEM!="block", GOTO="reenc_end"\n'
    printf 'ACTION!="add|change", GOTO="reenc_end"\n'
-   printf 'KERNEL!="%s", GOTO="reenc_end"\n' $REENC_DEV
+   printf 'KERNEL=="%s", ' $REENC_DEV
    printf 'ENV{ID_FS_TYPE}=="crypto_LUKS", RUN+="/sbin/initqueue \
-           --unique --onetime --name crypt-reencrypt-%%k \
+           --unique --onetime --settled --name crypt-reencrypt-%%k \
+           /sbin/reencrypt $env{DEVNAME} %s"\n' "$REENC_KEY $REENC_SLOT $REENC_SIZE"
+
+   printf 'ENV{ID_FS_UUID}=="*%s*", ' $REENC_DEV
+   printf 'ENV{ID_FS_TYPE}=="crypto_LUKS", RUN+="/sbin/initqueue \
+           --unique --onetime --settled --name crypt-reencrypt-%%k \
            /sbin/reencrypt $env{DEVNAME} %s"\n' "$REENC_KEY $REENC_SLOT $REENC_SIZE"
    printf 'LABEL="reenc_end"\n'
 } > /etc/udev/rules.d/69-reencryption.rules
