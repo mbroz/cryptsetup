48bf08922c8cca342717d327f1531241ddf64546
Ondrej Kozina <okozina@redhat.com>
2018-04-13 10:40:07 +0200

    Make all LUKS2 key size helpers return negative value on error.

--- lib/luks2/luks2_json_metadata.c
+++ lib/luks2/luks2_json_metadata.c
@@ -1763,13 +1763,13 @@ static int LUKS2_keyslot_get_volume_key_size(struct luks2_hdr *hdr, const char *
 	json_object *jobj1, *jobj2, *jobj3;
 
 	if (!json_object_object_get_ex(hdr->jobj, "keyslots", &jobj1))
-		return 0;
+		return -1;
 
 	if (!json_object_object_get_ex(jobj1, keyslot, &jobj2))
-		return 0;
+		return -1;
 
 	if (!json_object_object_get_ex(jobj2, "key_size", &jobj3))
-		return 0;
+		return -1;
 
 	return json_object_get_int(jobj3);
 }
