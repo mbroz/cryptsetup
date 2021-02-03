bb6d52219878f5b204a5e97c169fba76257a2e4f
Milan Broz <gmazyland@gmail.com>
2021-02-03 15:17:07 +0100

    Mark ssh token as experimental.

--- tokens/ssh/libcryptsetup-token-ssh.c
+++ tokens/ssh/libcryptsetup-token-ssh.c
@@ -1,8 +1,8 @@
 /*
- * Example of LUKS2 ssh token handler
+ * Example of LUKS2 ssh token handler (EXPERIMENTAL)
  *
- * Copyright (C) 2016-2020 Milan Broz <gmazyland@gmail.com>
- * Copyright (C) 2020 Vojtech Trefny
+ * Copyright (C) 2016-2021 Milan Broz <gmazyland@gmail.com>
+ * Copyright (C) 2020-2021 Vojtech Trefny
  *
  * Use:
  *  - generate LUKS device
