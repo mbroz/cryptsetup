b783aa034d94ce0fba2b99f0b4defd6759b15098
daniel.zatovic <daniel.zatovic@gmail.com>
2021-11-18 11:30:00 +0100

    fix

--- .gitlab/ci/cibuild-setup-ubuntu.sh
+++ .gitlab/ci/cibuild-setup-ubuntu.sh
@@ -17,6 +17,7 @@ sed -Ei 's/^deb /deb-src /' /etc/apt/sources.list~
 cat /etc/apt/sources.list~ >> /etc/apt/sources.list
 #bash -c "echo 'deb-src http://archive.ubuntu.com/ubuntu/ $RELEASE main restricted universe multiverse' >>/etc/apt/sources.list"
 
+apt-get -y update --fix-missing
 DEBIAN_FRONTEND=noninteractive apt-get -yq install software-properties-common
 
 # Latest gcc stack deb packages provided by
@@ -24,6 +25,5 @@ DEBIAN_FRONTEND=noninteractive apt-get -yq install software-properties-common
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 PACKAGES+=(gcc-$COMPILER_VERSION)
 
-apt-get -y update --fix-missing
 DEBIAN_FRONTEND=noninteractive apt-get -yq install "${PACKAGES[@]}"
 apt-get -y build-dep cryptsetup
