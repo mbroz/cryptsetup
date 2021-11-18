483bb247f6ddaa3bfe950c5723c6112fa905c043
daniel.zatovic <daniel.zatovic@gmail.com>
2021-11-18 11:28:45 +0100

    fix

--- .gitlab/ci/cibuild-setup-ubuntu.sh
+++ .gitlab/ci/cibuild-setup-ubuntu.sh
@@ -7,7 +7,6 @@ PACKAGES=(
 	gettext libssl-dev libdevmapper-dev libpopt-dev uuid-dev libsepol1-dev
 	libjson-c-dev libssh-dev libblkid-dev tar libargon2-0-dev libpwquality-dev
 	sharutils dmsetup jq xxd expect keyutils netcat passwd openssh-client sshpass
-	software-properties-common
 )
 
 COMPILER="${COMPILER:?}"
@@ -17,6 +16,8 @@ grep -E '^deb' /etc/apt/sources.list > /etc/apt/sources.list~
 sed -Ei 's/^deb /deb-src /' /etc/apt/sources.list~
 cat /etc/apt/sources.list~ >> /etc/apt/sources.list
 #bash -c "echo 'deb-src http://archive.ubuntu.com/ubuntu/ $RELEASE main restricted universe multiverse' >>/etc/apt/sources.list"
+
+DEBIAN_FRONTEND=noninteractive apt-get -yq install software-properties-common
 
 # Latest gcc stack deb packages provided by
 # https://launchpad.net/~ubuntu-toolchain-r/+archive/ubuntu/test
