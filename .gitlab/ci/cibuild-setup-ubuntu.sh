a9bf78adc39a0a6643c8ae1be31144c87e1dcdac
daniel.zatovic <daniel.zatovic@gmail.com>
2021-11-23 14:52:10 +0100

    Remove LLVM repo script

--- .gitlab/ci/cibuild-setup-ubuntu.sh
+++ .gitlab/ci/cibuild-setup-ubuntu.sh
@@ -18,21 +18,25 @@ cat /etc/apt/sources.list~ >> /etc/apt/sources.list
 
 apt-get -y update --fix-missing
 DEBIAN_FRONTEND=noninteractive apt-get -yq install software-properties-common wget lsb-release
+RELEASE="$(lsb_release -cs)"
+
 if [[ $COMPILER == "gcc" ]]; then
   # Latest gcc stack deb packages provided by
   # https://launchpad.net/~ubuntu-toolchain-r/+archive/ubuntu/test
   add-apt-repository -y ppa:ubuntu-toolchain-r/test
+  PACKAGES+=(gcc-$COMPILER_VERSION)
 elif [[ $COMPILER == "clang" ]]; then
-  .gitlab/ci/llvm.sh $COMPILER_VERSION
+  wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+  add-apt-repository "deb http://apt.llvm.org/${RELEASE}/   llvm-toolchain-${RELEASE}-${COMPILER_VERSION} main"
+
   # scan-build
-  PACKAGES+=(clang-tools-$COMPILER_VERSION)
+  PACKAGES+=(clang-tools-$COMPILER_VERSION clang-$COMPILER_VERSION lldb-$COMPILER_VERSION lld-$COMPILER_VERSION clangd-$COMPILER_VERSION)
   PACKAGES+=(perl)
 else
   exit 1
 fi
 
-PACKAGES+=(${COMPILER}-$COMPILER_VERSION)
-
+apt-get -y update --fix-missing
 DEBIAN_FRONTEND=noninteractive apt-get -yq install "${PACKAGES[@]}"
 apt-get -y build-dep cryptsetup
 
