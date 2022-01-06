47a1d9a6d6609dd3217f4195157472fe21e841b0
Ondrej Kozina <okozina@redhat.com>
2022-01-06 14:45:20 +0100

    Expose json_segment_contains_flag to internal library.

--- lib/luks2/luks2_internal.h
+++ lib/luks2/luks2_internal.h
@@ -263,6 +263,7 @@ uint64_t json_segments_get_minimal_offset(json_object *jobj_segments, unsigned b
 json_object *json_segment_create_linear(uint64_t offset, const uint64_t *length, unsigned reencryption);
 json_object *json_segment_create_crypt(uint64_t offset, uint64_t iv_offset, const uint64_t *length, const char *cipher, uint32_t sector_size, unsigned reencryption);
 int json_segments_segment_in_reencrypt(json_object *jobj_segments);
+bool json_segment_contains_flag(json_object *jobj_segment, const char *flag_str, size_t len);
 
 int LUKS2_assembly_multisegment_dmd(struct crypt_device *cd,
 	struct luks2_hdr *hdr,
