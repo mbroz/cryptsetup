5186f49613435f470f096881b806af4c9d30d68c
Milan Broz <gmazyland@gmail.com>
2022-11-13 11:08:23 +0100

    tests: fix compilation warnings with C18
    Fix the function prototype and define GNU source definition to get strdup().

--- tests/fake_systemd_tpm_path.c
+++ tests/fake_systemd_tpm_path.c
@@ -1,6 +1,9 @@
 #include <string.h>
 #include <stdlib.h>
 
+/* systemd tpm2-util.h */
+int tpm2_find_device_auto(int log_level, char **ret);
+
 extern int tpm2_find_device_auto(int log_level __attribute__((unused)), char **ret)
 {
 	const char *path = getenv("TPM_PATH");
