114a13af843cd5119958e453cdd35f046147e3bb
Daniel Zatovic <daniel.zatovic@gmail.com>
2022-11-29 18:25:13 +0100

    Add support for meson build system.
    For now, let's keep support for both - autotools and meson.

---
+++ man/meson_dist_convert.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# generates manpages from AsciiDoc files when building dist tarball
+# run asciidoctor in parallel on `nproc` cores
+
+set -e
+
+[ -z "$MESON_DIST_ROOT" ] && echo "This script is meant to be run only from meson while generating dist tarball." && exit 1
+
+if [ $# -lt 3 ]; then
+        echo "Usage: $0 <asciidoctor path> <release version> <adocfiles>"
+        exit 1
+fi
+
+ASCIIDOCTOR="$1"
+RELEASE_VERSION="$2"
+shift 2
+
+cd $MESON_DIST_ROOT/man
+i=1
+N=$(nproc)
+for adocfile in "$@"
+do
+    $ASCIIDOCTOR -b manpage --failure-level ERROR -a release-version=$RELEASE_VERSION --base-dir=$MESON_DIST_ROOT $adocfile &
+    if (( $i % $N == 0 )); then wait; fi
+    i=$((i+1))
+done
