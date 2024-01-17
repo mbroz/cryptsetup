cac3184da3573986db1056b90dc2a61b0209b0c9
Milan Broz <gmazyland@gmail.com>
2024-01-17 10:41:31 +0100

    Add a few tainted data info for coverity to avoid warnings.
    If sysconf is lying, then anything can happen.
    But check for overflow anyway.
    
    Device/partition offset overflow for IV can only cause
    bad decryption (expected).

--- lib/utils.c
+++ lib/utils.c
@@ -45,34 +45,44 @@ unsigned crypt_cpusonline(void)
 uint64_t crypt_getphysmemory_kb(void)
 {
 	long pagesize, phys_pages;
-	uint64_t phys_memory_kb;
+	uint64_t phys_memory_kb, page_size_kb;
 
 	pagesize = sysconf(_SC_PAGESIZE);
 	phys_pages = sysconf(_SC_PHYS_PAGES);
 
-	if (pagesize < 0 || phys_pages < 0)
+	if (pagesize <= 0 || phys_pages <= 0)
 		return 0;
 
-	phys_memory_kb = pagesize / 1024;
-	phys_memory_kb *= phys_pages;
+	page_size_kb = pagesize / 1024;
+	phys_memory_kb = page_size_kb * phys_pages;
 
+	/* sanity check for overflow */
+	if (phys_memory_kb / phys_pages != page_size_kb)
+		return 0;
+
+	/* coverity[return_overflow:FALSE] */
 	return phys_memory_kb;
 }
 
 uint64_t crypt_getphysmemoryfree_kb(void)
 {
 	long pagesize, phys_pages;
-	uint64_t phys_memoryfree_kb;
+	uint64_t phys_memoryfree_kb, page_size_kb;
 
 	pagesize = sysconf(_SC_PAGESIZE);
 	phys_pages = sysconf(_SC_AVPHYS_PAGES);
 
-	if (pagesize < 0 || phys_pages < 0)
+	if (pagesize <= 0 || phys_pages <= 0)
 		return 0;
 
-	phys_memoryfree_kb = pagesize / 1024;
-	phys_memoryfree_kb *= phys_pages;
+	page_size_kb = pagesize / 1024;
+	phys_memoryfree_kb = page_size_kb * phys_pages;
 
+	/* sanity check for overflow */
+	if (phys_memoryfree_kb / phys_pages != page_size_kb)
+		return 0;
+
+	/* coverity[return_overflow:FALSE] */
 	return phys_memoryfree_kb;
 }
 
