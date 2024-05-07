661f57def4e3aa158305d92079ced68d64be6d8a
Milan Broz <gmazyland@gmail.com>
2024-05-07 15:49:56 +0200

    Use SPDX license identifiers.
    This patch switches code to SPDX one-line license identifiers according to
    https://spdx.dev/learn/handling-license-info/
    and replacing long license text headers.
    
    I used C++ format on the first line in style
    // SPDX-License-Identifier: <id>
    except exported libcryptsetup.h, when only C comments are used.
    
    The only additional changes are:
    - switch backend utf8.c from LGPL2+ to LGPL2.1+ (as in systemd)
    - add some additional formatting lines.

--- lib/crypto_backend/crypto_openssl.c
+++ lib/crypto_backend/crypto_openssl.c
@@ -1,31 +1,9 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later WITH cryptsetup-OpenSSL-exception
 /*
  * OPENSSL crypto backend implementation
  *
  * Copyright (C) 2010-2024 Red Hat, Inc. All rights reserved.
  * Copyright (C) 2010-2024 Milan Broz
- *
- * This file is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This file is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this file; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * In addition, as a special exception, the copyright holders give
- * permission to link the code of portions of this program with the
- * OpenSSL library under certain conditions as described in each
- * individual source file, and distribute linked combinations
- * including the two.
- *
- * You must obey the GNU Lesser General Public License in all respects
- * for all of the code used other than OpenSSL.
  */
 
 #include <stdio.h>
--- tokens/ssh/ssh-utils.h
+++ tokens/ssh/ssh-utils.h
@@ -1,22 +1,9 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
 /*
  * ssh plugin utilities
  *
  * Copyright (C) 2016-2024 Milan Broz
  * Copyright (C) 2020-2024 Vojtech Trefny
- *
- * This file is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This file is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this file; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #ifndef SSH_UTILS_H
