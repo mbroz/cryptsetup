mbroz/cryptsetup

commit cf630f578d84e9e9af51a7416df1914f28b19378

Create underlying TCRYPT devices with CRYPT_SUBDEV prefix.
Create lower level TCRYPT device (underneath the toplevel one)
with CRYPT_SUBDEV prefix so that in later release we
can use general dependecies deactivation code.

The newly activated stacked TCRYPT devices will not correctly
deactivate with older pre 2.8.0 release.

lib/tcrypt/tcrypt.c
@@ -902,7 +902,7 @@ int TCRYPT_activate(struct crypt_device *cd,
 
 		log_dbg(cd, "Trying to activate TCRYPT device %s using cipher %s.",
 			dm_name, dmd.segment.u.crypt.cipher);
-		r = dm_create_device(cd, dm_name, CRYPT_TCRYPT, &dmd);
+		r = dm_create_device(cd, dm_name, i == 1 ? CRYPT_TCRYPT : CRYPT_SUBDEV, &dmd);
 
 		dm_targets_free(cd, &dmd);
 		device_free(cd, device);
@@ -941,7 +941,9 @@ static int TCRYPT_remove_one(struct crypt_device *cd, const char *name,
 		return r;
 
 	r = dm_query_device(cd, dm_name, DM_ACTIVE_UUID, &dmd);
-	if (!r && !strncmp(dmd.uuid, base_uuid, strlen(base_uuid)))
+	if (!r &&
+	    (!strncmp(dmd.uuid, base_uuid, strlen(base_uuid)) ||
+	     !dm_uuid_cmp(dmd.uuid, strchr(base_uuid, '-'))))
 		r = dm_remove_device(cd, dm_name, flags);
 
 	free(CONST_CAST(void*)dmd.uuid);
@@ -963,6 +965,7 @@ int TCRYPT_deactivate(struct crypt_device *cd, const char *name, uint32_t flags)
 	if (r < 0)
 		goto out;
 
+	/* FIXME: replace with dependency based deactivation (CRYPT_SUBDEV) in later releases */
 	r = TCRYPT_remove_one(cd, name, dmd.uuid, 1, flags);
 	if (r < 0)
 		goto out;

commit 6c7c8d36bbeb2370e377313a216177e208901966

Fix bug in parsing of tcrypt device in crypt_init_by_name.
When TCRYPT subdevices was moved to use SUBDEV dm uuid prefix
we did not correctly modified the TCRYPT_status_one routine
to correctly expect the SUBDEV uuid prefix.

Fixes: cf630f578d84
Fixes: #952.

lib/tcrypt/tcrypt.c
@@ -1023,7 +1023,7 @@ static int TCRYPT_status_one(struct crypt_device *cd, const char *name,
 
 	r = 0;
 
-	if (!strncmp(dmd.uuid, base_uuid, strlen(base_uuid))) {
+	if (is_tcrypt_subdev(dmd.uuid, base_uuid)) {
 		if ((c = strchr(tgt->u.crypt.cipher, '-')))
 			*c = '\0';
 		strcat(cipher, "-");

tests/api-test.c
@@ -1848,6 +1848,15 @@ static void TcryptTest(void)
 	OK_(crypt_activate_by_volume_key(cd, CDEVICE_1, NULL, 0, CRYPT_ACTIVATE_READONLY));
 	CRYPT_FREE(cd);
 
+	// Check tcrypt can correctly parse parameters from active stacked cipher device
+	OK_(crypt_init_by_name(&cd, CDEVICE_1));
+	OK_(strcmp(crypt_get_cipher(cd), "aes-twofish-serpent"));
+	OK_(strcmp(crypt_get_cipher_mode(cd), "xts-plain64"));
+	EQ_(crypt_get_volume_key_size(cd), 192);
+	/* just check if it correctly parsed data device down to a loopback device */
+	OK_(strncmp(crypt_get_device_name(cd) ?: "", "/dev/loop", 9));
+	CRYPT_FREE(cd);
+
 	// Deactivate the whole chain
 	EQ_(crypt_status(NULL, CDEVICE_1 "_1"), CRYPT_BUSY);
 	OK_(crypt_deactivate(NULL, CDEVICE_1));