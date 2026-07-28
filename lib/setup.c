2fb52062e50300dbb20f02c32eeb529464a33a7f
Milan Broz <gmazyland@gmail.com>
2026-07-28 12:00:43 +0200

    Initialize temporary header in crypt_header_restore early
    The temporary headers can be initialized on stack,
    no need to call memset later.

--- lib/setup.c
+++ lib/setup.c
@@ -4079,8 +4079,8 @@ int crypt_header_restore(struct crypt_device *cd,
 			 const char *backup_file)
 {
 	struct device *backup_device = NULL;
-	struct luks_phdr hdr1;
-	struct luks2_hdr hdr2;
+	struct luks_phdr hdr1 = {0};
+	struct luks2_hdr hdr2 = {0};
 	int r, version;
 
 	if (requested_type && !isLUKS(requested_type))
@@ -4108,8 +4108,6 @@ int crypt_header_restore(struct crypt_device *cd,
 		log_err(cd, _("Header backup file does not contain compatible LUKS header."));
 		return -EINVAL;
 	}
-
-	memset(&hdr2, 0, sizeof(hdr2));
 
 	if (!cd->type) {
 		if (version == 1)
