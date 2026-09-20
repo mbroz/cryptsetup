3a43658590dfe8bcd55f6c16312be0f5114b780f
Milan Broz <gmazyland@gmail.com>
2026-09-20 21:46:42 +0200

    Replace crypt_cipher_check_kernel with crypt_cipher_aead_known
    With deprecation of AF_ALG, we cannot rely on AEAD check through it.
    
    Just add simple check for known AEAD patterns (used only in LUKS2).
    Activation still can fail later, but that can happen aldo if kernel
    config changes.

--- lib/crypto_backend/cipher_generic.c
+++ lib/crypto_backend/cipher_generic.c
@@ -12,6 +12,10 @@
 #include <fcntl.h>
 #include "crypto_backend.h"
 
+#ifndef ARRAY_SIZE
+# define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+#endif
+
 struct cipher_alg {
 	const char *name;
 	const char *mode;
@@ -43,6 +47,50 @@ static const struct cipher_alg cipher_algs[] = {
 	{ NULL,          NULL,  0, false }
 };
 
+struct cipher_aead_alg {
+	const char *name;
+	const char *mode;
+	const char *integrity;
+	size_t key_bytes;
+};
+
+static const struct cipher_aead_alg cipher_aead_algs[] = {
+	{ "aes",      "gcm", "aead", 16 },
+	{ "aes",      "gcm", "aead", 24 },
+	{ "aes",      "gcm", "aead", 32 },
+	{ "aes",      "ccm", "aead", 16 + 3 }, // CCM used as RFC 4309, 3 bytes for fixed salt
+	{ "aes",      "ccm", "aead", 24 + 3 },
+	{ "aes",      "ccm", "aead", 32 + 3 },
+	{ "serpent",  "gcm", "aead", 16 },
+	{ "serpent",  "gcm", "aead", 24 },
+	{ "serpent",  "gcm", "aead", 32 },
+	{ "serpent",  "ccm", "aead", 16 + 3 },
+	{ "serpent",  "ccm", "aead", 24 + 3 },
+	{ "serpent",  "ccm", "aead", 32 + 3 },
+	{ "twofish",  "gcm", "aead", 16 },
+	{ "twofish",  "gcm", "aead", 24 },
+	{ "twofish",  "gcm", "aead", 32 },
+	{ "twofish",  "ccm", "aead", 16 + 3 },
+	{ "twofish",  "ccm", "aead", 24 + 3 },
+	{ "twofish",  "ccm", "aead", 32 + 3 },
+	{ "aria",     "gcm", "aead", 16 },
+	{ "aria",     "gcm", "aead", 24 },
+	{ "aria",     "gcm", "aead", 32 },
+	{ "aria",     "ccm", "aead", 16 + 3 },
+	{ "aria",     "ccm", "aead", 24 + 3 },
+	{ "aria",     "ccm", "aead", 32 + 3 },
+	{ "camellia", "gcm", "aead", 16 },
+	{ "camellia", "gcm", "aead", 24 },
+	{ "camellia", "gcm", "aead", 32 },
+	{ "camellia", "ccm", "aead", 16 + 3 },
+	{ "camellia", "ccm", "aead", 24 + 3 },
+	{ "camellia", "ccm", "aead", 32 + 3 },
+	{ "sm4",      "gcm", "aead", 16 },
+	{ "sm4",      "ccm", "aead", 16 + 3 },
+	{ "aegis128", NULL,  "aead", 16 },
+	{ "chacha20", NULL,  "poly1305", 32 }, // Used as RFC 7539
+};
+
 static const struct cipher_alg *_get_alg(const char *name, const char *mode)
 {
 	int i = 0;
@@ -55,6 +103,56 @@ static const struct cipher_alg *_get_alg(const char *name, const char *mode)
 		i++;
 	}
 	return NULL;
+}
+
+static bool aead_iv_known(const char *iv)
+{
+	if (!iv)
+		return false;
+
+	if (!strcasecmp(iv, "random"))
+		return true;
+
+	/* These are not secure for AEAD, but sometimes used */
+	if (!strcasecmp(iv, "plain64") || !strcasecmp(iv, "plain64be"))
+		return true;
+
+	return false;
+}
+
+bool crypt_cipher_aead_known(const char *name, const char *mode, const char *integrity, size_t key_bytes)
+{
+	size_t i, mode_len;
+
+	if (!name || !mode || !integrity)
+		return false;
+
+	for (i = 0; i < ARRAY_SIZE(cipher_aead_algs); i++) {
+		if (strcasecmp(name, cipher_aead_algs[i].name))
+			continue;
+
+		if (strcasecmp(integrity, cipher_aead_algs[i].integrity))
+			continue;
+
+		/* Compare only mode for ciphers that use a mode */
+		if (cipher_aead_algs[i].mode) {
+			mode_len = strlen(cipher_aead_algs[i].mode);
+			if (strncasecmp(mode, cipher_aead_algs[i].mode, mode_len))
+				continue;
+			if (mode[mode_len] != '-')
+				continue;
+			if (!aead_iv_known(&mode[mode_len + 1]))
+				continue;
+		} else if (!aead_iv_known(mode)) /* no mode, only IV */
+				continue;
+
+		if (key_bytes != cipher_aead_algs[i].key_bytes)
+			continue;
+
+		return true;
+	}
+
+	return false;
 }
 
 int crypt_cipher_ivsize(const char *name, const char *mode)
--- lib/crypto_backend/crypto_backend.h
+++ lib/crypto_backend/crypto_backend.h
@@ -108,6 +108,7 @@ int crypt_cipher_decrypt(struct crypt_cipher *ctx,
 			 const char *in, char *out, size_t length,
 			 const char *iv, size_t iv_length);
 bool crypt_cipher_kernel_only(struct crypt_cipher *ctx);
+bool crypt_cipher_aead_known(const char *name, const char *mode, const char *integrity, size_t key_bytes);
 
 /* Benchmark of kernel cipher performance */
 int crypt_cipher_perf_kernel(const char *name, const char *mode, char *buffer, size_t buffer_size,
